acc5a6c92509353e1e4a951a6a7be8fc361dcc22
Steven Nunez <steven.nunez@gmail.com>
2014-05-09 14:43:55 -0400

    Solution

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,10 @@
 def count_elements(array)
-  # code goes here
+  count = Hash.new(0)
+  array.each {|item| count[item] += 1 }
+  count
+
+  # Fancy
+  # array.each_with_object(Hash.new(0)) do |item, count|
+  #   count[item] += 1
+  # end
 end
- 
