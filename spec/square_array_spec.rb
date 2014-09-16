43b6a1cb2f3129fbcafa49fc1c9a3d59db665294
Katie Hoffman <ktahoffman@gmail.com>
2014-09-16 15:16:10 -0400

    fix spec

--- spec/square_array_spec.rb
+++ spec/square_array_spec.rb
@@ -1,6 +1,6 @@
 describe '#count_elements' do
 
-  it 'should return a hash with the count of each element in the array' do
+  it 'should return a hash with the count of each element in the simple array' do
     array = ['cat', 'dog', 'fish', 'fish']
     hash = count_elements(array)
     expect(hash['cat']).to eq(1)
@@ -8,12 +8,11 @@ describe '#count_elements' do
     expect(hash['fish']).to eq(2)
   end
 
-  it 'should return a hash with the count of each element in the array' do
+  it 'should return a hash with the count of each element in the larger array' do
     arr = ['sugar glider', 'sloth', 'sloth', 'sloth', 'pig']
-    counted = { 'sugar glider' => 1, 'sloth' => 3, 'pig' => 1 }
-    hash = count_elements(array)
+    hash = count_elements(arr)
     expect(hash['sugar glider']).to eq(1)
-    expect(hash['sloth']).to eq(1)
+    expect(hash['sloth']).to eq(3)
     expect(hash['pig']).to eq(1)
   end
 
