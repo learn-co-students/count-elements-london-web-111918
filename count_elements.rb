ac61adeddc6591eb9eefc277d1d90df6ee14bf76
IMH51 <ianmartinharrison@outlook.com>
2018-11-20 14:27:49 +0000

    method returns hash with count of animals in array

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,10 @@
 def count_elements(array)
-  # code goes here
+  count_hash = {}
+  array.each do |animal|
+    count_hash[animal] = 0
+  end
+  array.each do |animal|
+    count_hash[animal] += 1
+  end
+  count_hash
 end
- 
